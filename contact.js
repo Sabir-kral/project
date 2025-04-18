e71b37f4aaac4b630e11ddfdaa5631921b0db8a0
Sabir-kral <sabirmemmedli21152014@gmail.com>
2025-04-18 20:57:19 +0400

    day6

---
+++ contact.js
@@ -0,0 +1,22 @@
+let user = localStorage.getItem("activeUsers")
+let names = document.querySelector(".name")
+names.innerHTML = user
+names.style.display = "inline"
+let person = document.querySelector(".bi-person")
+person.style.display = "inline"
+function opens() {
+    window.open("./login.html")
+}
+function out() {
+    person.style.display = "none"
+    names.style.display = "none"
+    let logOut = document.querySelector(".logOut")
+    let logIn = document.querySelector(".logIn")
+    logOut.style.display = "none"
+    logIn.style.display = "inline"
+    localStorage.removeItem("activeUsers")
+    logOut.style.display = "none"
+}
+function openss() {
+    window.open("./user.html")
+}
